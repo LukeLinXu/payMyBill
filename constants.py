676cba2c3c35e3f7828f558998ac6927f411af48
llin <dev.linxu@gmail.com>
2016-03-25 10:08:19 -0400

    add constants

---
+++ constants.py
@@ -0,0 +1,13 @@
+
+
+ENBRIDGE_ACCOUNT = 'xxxxx@xxxx.xxx'
+ENBRIDGE_PASSWORD = 'xxxxxxx'
+
+RBC_ACCOUNT = 'xxxxx@xxxx.xxx'
+RBC_PASSWORD = 'xxxxxxx'
+
+MBNA_ACCOUNT = 'xxxxx@xxxx.xxx'
+MBNA_PASSWORD = 'xxxxxxx'
+
+COSTCO_ACCOUNT = 'xxxxx@xxxx.xxx'
+COSTCO_PASSWORD = 'xxxxxxx'

bb0e3f974f6e604c3c535db09ea9f49c77d4d1b2
llin <dev.linxu@gmail.com>
2016-03-25 23:13:56 -0400

    done rbc part

--- payMyBill.py
+++ payMyBill.py
@@ -1,27 +1,29 @@
 import time
 from splinter import Browser
 
+import constants
 import fileUtils
 import grapCostcobill
 import grapGasbill
 import grapMBNAbill
 
 
-def login_rbc(browser):
-    url = "http://www.rbc.com/canada.html"
-    browser.visit(url)
-    button = browser.find_by_text('Sign In ')
-    button.click()
-    time.sleep(50)
+def login_rbc():
+    profile = fileUtils.getBroswerProfile()
+    with Browser(profile_preferences=profile) as browser:
+        browser.visit('http://www.rbc.com/canada.html')
+        browser.find_by_text('Sign In ').click()
+        browser.find_by_id('K1').fill(constants.RBC_ACCOUNT)
+        browser.find_by_id('Q1').fill(constants.RBC_PASSWORD)
+        browser.find_by_text('Sign In').click()
+        browser.click_link_by_partial_text('RBC Shoppers Optimum')
+        value = browser.find_by_xpath('''//tr[@class='ccadTableBold']''').last.find_by_tag('td').value
+        return value
 
 
 
 if __name__ == '__main__':
     # print('MBNA', grapMBNAbill.get_bill_number())
-    print('Costco', grapCostcobill.get_bill_number())
-    print('Enbridge', grapGasbill.get_bill_number())
-    pass
-    # with Browser('chrome') as browser:
-        # grapMBNAbill.get_bill_number()
-        # grapCostcobill.get_bill_number()
-        # login_rbc(browser)
+    # print('Costco', grapCostcobill.get_bill_number())
+    # print('Enbridge', grapGasbill.get_bill_number())
+    print('RBC Credit card', login_rbc())

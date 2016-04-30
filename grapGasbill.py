fd5c9499ce92d4db439c6d4a38bb4b2b3496ee0f
llin <dev.linxu@gmail.com>
2016-04-30 10:20:02 -0400

    fix some bug for enbridge website change

--- grapGasbill.py
+++ grapGasbill.py
@@ -12,7 +12,9 @@ def get_bill_number():
         browser.find_by_id('ctl00_BodyContent_ctrlLogin1_login_Password').fill(constants.ENBRIDGE_PASSWORD)
         button = browser.find_by_text('SIGN IN')
         button.click()
-        browser.find_by_id('ctl00_ctl00_BodyContent_ContentPlaceHolder1_lnkViewCurrentBill').click()
+        browser.find_by_id('Extranet_aMyEnbridgeOverview').click()
+        browser.find_by_id('ctl00_ctl00_BodyContent_ContentPlaceHolder1_liViewCurrentBill').click()
+        # browser.find_by_id('ctl00_ctl00_BodyContent_ContentPlaceHolder1_lnkViewCurrentBill').click()
         value = browser.find_by_id('ctl00_Main_lvBills_ctrl0_rptCatalog_ctl05_lblContent').value
         datelink = browser.find_by_id('ctl00_Main_lvBills_ctrl0_rptCatalog_ctl03_lnkFileLink')
         datelinkvalue = datelink.value.split('/')
